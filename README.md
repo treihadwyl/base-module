fc97f8e212e8211943e40294976a8d4a6bf3972c
Matt Styles <matt@veryfizzyjelly.com>
2014-03-30 21:39:17 +0100

    initial scaffold

---
+++ README.md
@@ -0,0 +1,11 @@
+# Treihadwyl Basic Package Scaffold
+
+> Base module for building component packages
+
+## Installation
+
+Use [tpm](https://github.com/treihadwyl/tpm) to scaffold a new module.
+
+```
+tpm create package
+```
---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+/**
+ * Treihadwyl Basic Package Scaffold
+ * Copyright © 2014 Matt Styles <matt@veryfizzyjelly.com>
+ * Licensed under the ISC license
+ * ---
+ *
+ * Just runs tests
+ * Building is handled by core
+ */
+
+'use strict';
+
+var gulp = require( 'gulp' ),
+    mocha = require( 'gulp-mocha' ),
+    plumber = require( 'gulp-plumber' ),
+    notify = require( 'gulp-notify' ),
+    gutil = require( 'gulp-util' );
+
+
+gulp.task( 'test', function() {
+    return gulp
+        .src( 'tests/spec/**/*.js' )
+        .pipe( plumber( {
+            errorHandler: notify.onError( 'Test error: <%= error.message %>' )
+        }))
+        .pipe( mocha({
+            reporter: 'nyan',
+            ui: 'tdd'
+        }))
+        .on( 'error', function( err ) {
+            gutil.log( gutil.colors.red( err.message ) );
+            this.emit( 'end' );
+        });
+});
+
+
+gulp.task( 'default', [ 'test' ], function() {
+    gutil.log( 'Finished running tests' );
+});
